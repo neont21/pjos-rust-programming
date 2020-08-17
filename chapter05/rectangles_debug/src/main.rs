2f7b2c26226639ae64664cd55220a0e53c473df5
Peter J <peter.j@kakao.com>
2020-08-17 23:49:15 +0900

    chapter05 rectangles debug (1)

---
+++ chapter05/rectangles_debug/src/main.rs
@@ -0,0 +1,13 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect: {}", rect);
+}
