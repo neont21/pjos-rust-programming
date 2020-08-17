782a85cde70d675212d96bebc88edcebbefd074d
Peter J <peter.j@kakao.com>
2020-08-18 01:08:43 +0900

    chapter05 rectangles method

---
+++ chapter05/rectangles_method/src/main.rs
@@ -0,0 +1,22 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
