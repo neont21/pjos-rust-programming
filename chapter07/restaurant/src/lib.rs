3f9516c3eed5436010276abd5f621600426d699b
Peter J <peter.j@kakao.com>
2020-08-19 08:00:17 +0900

    chapter07 restaurant (4)

--- chapter07/restaurant/src/lib.rs
+++ chapter07/restaurant/src/lib.rs
@@ -15,6 +15,25 @@ mod front_of_house {
 }
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::front_of_house::serving::serve_order();
@@ -24,7 +43,11 @@ mod back_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-
     front_of_house::hosting::add_to_waitlist();
+    
+    let appetizer = back_of_house::Appetizer::Salad;
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheet");
+    println!("I'd like {} toast please", meal.toast);
 }
