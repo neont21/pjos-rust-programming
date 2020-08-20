49c0e1eadca7e68790730f7e08b55ab644732433
Peter J <peter.j@kakao.com>
2020-08-20 20:18:03 +0900

    chapter08 hashmap collect (1)

---
+++ chapter08/hashmap_collect/src/main.rs
@@ -0,0 +1,15 @@
+use std:: collections::HashMap;
+
+fn main() {
+    let teams = vec![
+        String::from("Blue"),
+        String::from("Yellow"),
+    ];
+    let initial_scores = vec![
+        10,
+        50,
+    ];
+
+    let mut score: HashMap<_, _> =
+        teams.iter().zip(initial_scores.iter()).collect();
+}
