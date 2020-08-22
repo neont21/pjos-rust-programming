bfd82538ee70082b5adf30d0d5e262801eb380b8
Peter J <peter.j@kakao.com>
2020-08-23 06:10:10 +0900

    chapter10 largest generic (2)

--- chapter10/largest_generic/src/main.rs
+++ chapter10/largest_generic/src/main.rs
@@ -1,23 +1,12 @@
 fn main() {
     let number_list = vec![34, 50, 35, 100, 65];
-    println!("The largest number is {}", largest_i32(&number_list));
+    println!("The largest number is {}", largest(&number_list));
 
     let character_list = vec!['y', 'm', 'a', 'q'];
-    println!("The largest character is {}", largest_char(&character_list));
+    println!("The largest character is {}", largest(&character_list));
 }
 
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for item in list {
-        if *item > largest {
-            largest = *item;
-        }
-    }
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for item in list {
