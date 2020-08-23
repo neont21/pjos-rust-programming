12524bb5757fa8e701e7371f1e0c209d50b3e581
Peter J <peter.j@kakao.com>
2020-08-24 02:25:05 +0900

    chapter10 summary trait (4)

--- chapter10/summary_trait/src/main.rs
+++ chapter10/summary_trait/src/main.rs
@@ -1,5 +1,4 @@
-use summary_trait::Summary;
-use summary_trait::Tweet;
+use summary_trait::{Summary, Tweet, NewsArticle};
 
 fn main() {
     let tweet = Tweet {
@@ -10,4 +9,14 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Peter broken Hazzi's Windows"),
+        location: String::from("Gwangjin-goo, Seoul, Korea"),
+        author: String::from("Ferris"),
+        content: String::from("Finally, Peter broken Hazzi's Windows, \
+            and install Linux on Hazzi's laptop."),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
