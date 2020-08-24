5e157c1ac5d52783d81961749e4bea07570ecd63
Peter J <peter.j@kakao.com>
2020-08-25 05:26:21 +0900

    chapter10 longest string (1)

---
+++ chapter10/longest_string/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    println!("The longest string is {}", longest(string1.as_str(), string2));
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
