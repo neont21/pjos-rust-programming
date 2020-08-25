362ff1937f7868798633209724738fe863e5506e
Peter J <peter.j@kakao.com>
2020-08-26 03:37:34 +0900

    chapter10 important excerpt (2)

--- chapter10/important_excerpt/src/main.rs
+++ chapter10/important_excerpt/src/main.rs
@@ -2,6 +2,13 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
 fn main() {
     let novel = String::from("Call me Ismael. Some years ago...");
     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
@@ -10,4 +17,7 @@ fn main() {
     };
 
     println!("{}", i.part);
+
+    let return_value = i.announce_and_return_part("test");
+    println!("{}", return_value);
 }
