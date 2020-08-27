2de31876a22d2d3baf50327b61571e4ffab0ab73
Peter J <peter.j@kakao.com>
2020-08-27 18:46:49 +0900

    chapter11 rectangle (1)

---
+++ chapter11/rectangle/src/lib.rs
@@ -0,0 +1,43 @@
+pub struct Rectanlge {
+    length: u32, 
+    width: u32,
+}
+
+impl Rectanlge {
+    pub fn can_hold(&self, other: &Rectanlge) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectanlge {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectanlge {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectanlge {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectanlge {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
