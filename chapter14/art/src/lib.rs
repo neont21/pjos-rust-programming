3cadedda0d034ad600541c527a7aa8f12422e596
Peter J <peter.j@kakao.com>
2020-09-09 17:20:42 +0900

    chapter14 art (2)

--- chapter14/art/src/lib.rs
+++ chapter14/art/src/lib.rs
@@ -1,7 +1,11 @@
 //! # Art
 //! 
 //! A library for modeling artistic concepts.
- 
+
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
 pub mod kinds {
     /// The primary colors according to the RYB color model
     pub enum PrimaryColor {
