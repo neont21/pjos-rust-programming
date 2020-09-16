15694186db55fe2550bb4fd3ff8746935971da28
Peter J <peter.j@kakao.com>
2020-09-16 16:44:23 +0900

    chapter15 mock object (2)

--- chapter15/mock_object/src/lib.rs
+++ chapter15/mock_object/src/lib.rs
@@ -32,3 +32,36 @@ impl<'a, T> LimitTracker<'a, T>
             }
         }
     }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockMessenger {
+        sent_message: Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_message: vec![],
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_message.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_message.len(), 1);
+    }
+}
