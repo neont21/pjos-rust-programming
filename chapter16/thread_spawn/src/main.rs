e677998de9104e09adccee6f1de1c1a56e7e4e5d
Peter J <peter.j@kakao.com>
2020-09-18 10:40:39 +0900

    chapter16 thread spawn (2)

--- chapter16/thread_spawn/src/main.rs
+++ chapter16/thread_spawn/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(move || {
+    let handle = thread::spawn(move || {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -12,4 +12,6 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
