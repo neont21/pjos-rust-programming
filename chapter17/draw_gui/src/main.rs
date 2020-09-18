0dd79a871f07119b94547fc28583a873459500d5
Peter J <peter.j@kakao.com>
2020-09-19 01:52:10 +0900

    chapter17 draw gui

---
+++ chapter17/draw_gui/src/main.rs
@@ -0,0 +1,39 @@
+use draw_gui::{Draw, Screen, Button};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("draw select box (size: {} * {})", self.width, self.height);
+        for item in &self.options {
+            println!("- option: {}", item);
+        }
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("Ok"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
