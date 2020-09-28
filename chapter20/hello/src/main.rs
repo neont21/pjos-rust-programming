9e49513f8bfe9bb4cc5b4b05d6dde5030d0fd7da
Peter J <peter.j@kakao.com>
2020-09-29 00:01:20 +0900

    chapter20 hello (7) simulate delay issue

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+use std::time::Duration;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -21,8 +23,12 @@ fn handle_connection(mut stream: TcpStream) {
     //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
